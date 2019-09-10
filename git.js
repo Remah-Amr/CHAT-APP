9f8a73ae4c2b59cebaf27486d8b7652a61f658be
remah amr <remah3335@gmail.com>
2019-09-11 00:06:19 +0200

    any

---
+++ git.js
@@ -0,0 +1,11 @@
+// when you want to add something without loss original folder or instead of to copy the folder
+// you can remote to another branch then write what you want by:
+// git branch name
+// git checkout name
+// do every thing or change you want
+// git add .
+// git commit -am 'name'
+// then : git checkout master
+// git merge name
+
+// you can write git status to know which branch you in
